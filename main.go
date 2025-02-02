9c4be85d3a333a24e783615a4fd7689db3e5aa39
Ankit varshney <lyfflame45@gmail.com>
2025-02-02 18:51:45 +0530

    getting html

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -10,14 +13,41 @@ func main() {
 	args := os.Args
 	argsWithoutProgramName := args[1:]
 	if len(argsWithoutProgramName) < 1 {
-		fmt.Println("No website provided ")
+		fmt.Println("no website provided ")
 		os.Exit(1)
 	}
 	if len(argsWithoutProgramName) > 1 {
-		fmt.Println("Too many arguments provided")
+		fmt.Println("too many arguments provided")
 		os.Exit(1)
 	}
 	baseUrl := argsWithoutProgramName[0]
-	fmt.Println("starting crawl of ", baseUrl	)
+	fmt.Println("starting crawl of ", baseUrl)
+	html, err := getHTML(baseUrl)
+	if err != nil {
+		fmt.Println("error getting html: ", err)
+		os.Exit(1)
+	}
+	fmt.Println(html)
 
+}
+
+func getHTML(rawUrl string) (string, error) {
+	response, err := http.Get(rawUrl)
+	if err != nil {
+		return "", fmt.Errorf("error getting html: %w", err)
+	}
+	if response.StatusCode >= 400 {
+		return "", fmt.Errorf("error getting html: %s", response.Status)
+	}
+	contentType := response.Header.Get("Content-Type")
+	if !strings.Contains(contentType, "text/html") {
+		return "", fmt.Errorf("error getting content type: Content-Type header is missing")
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading html: %w", err)
+	}
+	return string(body), nil
 }
